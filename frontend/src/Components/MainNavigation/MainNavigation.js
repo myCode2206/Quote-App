2cb131d6041175066d7915ace2d4a4e78719768f
myCode2206 <rajatyt2206@gmail.com>
2024-04-03 12:55:17 +0530

    updated

---
+++ frontend/src/Components/MainNavigation/MainNavigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <nav className={styles.nav}>
+      <h2>Quotify</h2>
+      <ul>
+        <li>
+          <Link to="/">All Quotes</Link>
+        </li>
+        <li>
+          <Link to="/new">Add Quotes</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default MainNavigation;
---
+++ frontend/src/Components/Pages/ShowQuotes.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const ShowQuotes = () => {
+  const params = useParams();
+  const [quote, setQuote] = useState({
+    author: "",
+    text: "",
+  });
+
+  async function fetchAQuote() {
+    const res = await axios.get(`http://localhost:8000/quotes/${params.id}`);
+
+    const { author, text } = res.data;
+
+    setQuote({ author, text });
+  }
+
+  useEffect(() => {
+    fetchAQuote();
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>{quote.author}</h1>
+      <p>{quote.text}</p>
+    </div>
+  );
+};
+
+export default ShowQuotes;
